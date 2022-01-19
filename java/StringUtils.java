7760733409bf45dcc5a4b13d668fa12c237458fd
DIR\adrian.kiss <michael_adryan2006@yahoo.com>
2022-01-19 15:27:09 +0200

    Added Sustring class

--- java/StringUtils.java
+++ java/StringUtils.java
@@ -2,4 +2,16 @@ package java;
 
 public class StringUtils {
     
+
+public String sustring(int beginIndex) {    
+       if (beginIndex < 0) {    
+           throw new StringIndexOutOfBoundsException(beginIndex);    
+       }    
+       int subLen = value.length - beginIndex;    
+       if (subLen < 0) {    
+           throw new StringIndexOutOfBoundsException(subLen);    
+       }    
+       return (beginIndex == 0) ? this : new String(value, beginIndex, subLen);    
+   } 
+
 }
