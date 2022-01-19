cfda2def4ef47298fe82006fdbdd89c7591332a9
m.muresan <m.muresan@accenture.com>
2022-01-19 15:45:30 +0200

    reverse function

--- java/StringUtils.java
+++ java/StringUtils.java
@@ -2,4 +2,21 @@ package java;
 
 public class StringUtils {
     
+    public int reverse(int nr){
+
+        int reversed = 0;       
+        System.out.println("Original Number: " + nr);
+
+        // run loop until num becomes 0
+        while(nr != 0) {       
+            // get last digit from num
+            int digit = nr % 10;
+            reversed = reversed * 10 + digit;
+
+            // remove the last digit from num
+            nr /= 10;
+        }
+
+        return reversed;
+    }
 }
