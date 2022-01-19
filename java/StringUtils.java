88fad455e479bdf7efd79d50f2424f32ce282311
BeatriceNita <44348425+BeatriceNita@users.noreply.github.com>
2022-01-19 15:34:12 +0200

    palindrome() added

--- java/StringUtils.java
+++ java/StringUtils.java
@@ -2,4 +2,11 @@ package java;
 
 public class StringUtils {
     
+function palindrome(str) {
+  let lowStr = str.toLowerCase();
+  let reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr; 
+}
+
+
 }
